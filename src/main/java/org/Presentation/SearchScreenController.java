20950c69ab65f5b067407e021f8010d529948acc
chmad18 <chmad18@users.noreply.github.com>
2020-04-18 12:25:17 +0200

    Indsat flere klasser og lir, og tjekket programmet stadig kører

--- src/main/java/org/Presentation/SearchScreenController.java
+++ src/main/java/org/Presentation/SearchScreenController.java
@@ -1,24 +1,50 @@
 package org.Presentation;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import org.Data.DatabaseController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class SearchScreenController {
-
-    @FXML
-    private TextField textFieldSearch;
 
     @FXML
     private Button buttonSearch;
 
     @FXML
-    void buttonHandlerSearch(ActionEvent event) {
-        if (!textFieldSearch.getText().isEmpty()){
-//            DatabaseController databaseController = new DatabaseController();
-//            databaseController.searchProduction(textFieldSearch.getText());
+    private TextField textFieldSearchInput;
+
+    @FXML
+    void buttonHandlerSearch(ActionEvent event) throws IOException {
+        if (!textFieldSearchInput.getText().isEmpty()) {
+            Map<String, Integer> resultMap = DatabaseController.searchProduction(textFieldSearchInput.getText());
+            displaySearchResults(resultMap);
+
         }
+    }
+
+    public void displaySearchResults(Map<String, Integer> productionMap) throws IOException {
+        List<Hyperlink> hyperlinks = new ArrayList<>();
+        App.setRoot("DisplaySearchResultsGUI");
+
+        for (String title : productionMap.keySet()) {
+            Hyperlink hl = new Hyperlink(title);
+            hl.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+
+                }
+            });
+            hyperlinks.add(hl);
+        }
+
     }
 
 }
