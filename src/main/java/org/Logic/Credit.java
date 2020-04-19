5023985d7b5e76a9e43dfdf26ee7009ccb69dcf4
DanielNicolas <48410137+DanielNicolas@users.noreply.github.com>
2020-04-19 11:56:10 +0200

    Added ProductionManager, Production and Credit class

---
+++ src/main/java/org/Logic/Credit.java
@@ -0,0 +1,48 @@
+package org.Logic;
+
+public class Credit {
+    private Person person;
+    private Production production;
+    private String roleType;
+    private String roleName;
+
+
+    public Credit(Person person, Production production, String roleType, String roleName) {
+        this.person = person;
+        this.production = production;
+        this.roleType = roleType;
+        this.roleName = roleName;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Production getProduction() {
+        return production;
+    }
+
+    public void setProduction(Production production) {
+        this.production = production;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
