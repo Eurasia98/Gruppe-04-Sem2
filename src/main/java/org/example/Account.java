8be4dd6fcb8b663ff8972d3481f60a3665a665ba
Emilo1234 <31166672+Emilo1234@users.noreply.github.com>
2020-04-20 17:23:48 +0200

    Account klasse

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Account {
+
+    private String username;
+    private String password;
+    private int accountType;
+
+    public Account(String Username, String Password, int typeNum) {
+        this.username = Username;
+        this.password = Password;
+        this.accountType = typeNum;
+    }
+    Account
+    acc1 = new Account("Nybruger","password123", 1);
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAccountType() {
+        return accountType;
+    }
+
+}
