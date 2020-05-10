f830f97979e8dc0c283f21a83e0454742f62f20f
Madsen <chmad18@student.sdu.dk>
2020-05-10 19:18:33 +0200

    Lavet teststring

--- src/main/java/io/github/eurasia98/sem2/presentation/App.java
+++ src/main/java/io/github/eurasia98/sem2/presentation/App.java
@@ -19,6 +19,7 @@ public class App extends Application{
     private static Stage window;
     private static CreditSystem creditSystem = new CreditSystem();
     private static String searchField;
+    public String testString;
 
 
     public static String getSearchField() {
